519bbf1b102e891fbc622cc5448d7db6ff6f9ace
Álvaro Requejo <alvarorece@gmail.com>
2021-12-17 13:43:54 +0100

    searchservice unit test, readme, updated gitignore

---
+++ README.md
@@ -0,0 +1,13 @@
+# IMDB Search
+## Endpoints
+### Search endpoint
+```
+/search?query=
+```
+Returns a JSON with the following format:
+```JSON
+{
+    "query": "queryText",
+    "clusterName": "clusterName"
+}
+```
--- src/main/java/co/empathy/p01/app/ClusterNameUnavailableException.java
+++ src/main/java/co/empathy/p01/app/ClusterNameUnavailableException.java
@@ -4,4 +4,7 @@ public class ClusterNameUnavailableException extends Exception {
     public ClusterNameUnavailableException(Throwable ex) {
         super("We could not get the name from the cluster", ex);
     }
+    public ClusterNameUnavailableException() {
+        super("We could not get the name from the cluster");
+    }
 }
